b19342f714a6f89d5b7b47420e8e7ce52d307dcd
samorai <oleg.samorai@gmail.com>
2016-03-16 18:45:32 +0200

    add more middlewares for routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,7 +18,7 @@ $app->get('/', function () use ($app) {
 
 /** @var Nebo15\REST\Router $api */
 $api = $app->make('Nebo15\REST\Router');
-$api->api('groups', 'GroupsController', ['oauth', 'applicationable']);
+$api->api('groups', 'GroupsController', ['oauth', 'applicationable', 'has_access']);
 $api->api('tables', 'TablesController', ['oauth', 'applicationable', 'has_access']);
 
 
@@ -38,7 +38,7 @@ $app->group(
     [
         'prefix' => 'api/v1/admin',
         'namespace' => 'App\Http\Controllers',
-        'middleware' => ['oauth', 'applicationable']
+        'middleware' => ['oauth', 'applicationable', 'has_access']
     ],
     function ($app) use ($api) {
         /** @var Laravel\Lumen\Application $app */
