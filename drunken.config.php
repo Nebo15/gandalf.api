6b2266c6dbc81fe4696f6866d013be205ff1ac56
paul-nebo <paul@nebo15.com>
2016-06-10 18:10:31 +0300

    removed drunken config

--- drunken.config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'db' => 'gandalf_test',
-    'workers-dir' => __DIR__ . '/app/Drunken',
-    'tasks-clear-period' => '-24 hours',
-    'log_path' => __DIR__ . '/storage/logs/drunken.log',
-];
