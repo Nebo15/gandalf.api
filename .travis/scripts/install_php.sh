bf76fd6dc7e40e09989b0d7f1af10e275e5f0c5b
samorai <oleg.samorai@gmail.com>
2017-05-09 18:44:38 +0300

    pecl install mongo

--- .travis/scripts/install_php.sh
+++ .travis/scripts/install_php.sh
@@ -3,6 +3,7 @@ sudo touch /var/log/php-fpm.log
 sudo chmod 777 /var/log/php-fpm.log
 
 # Set custom config
+sudo pecl install mongo
 sudo ln -s $PWD/.travis/conf/php-fpm.conf ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
 echo 'always_populate_raw_post_data = -1' >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo 'date.timezone = "Europe/Kiev"' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
