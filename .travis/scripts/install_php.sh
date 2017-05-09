5f329b2195a9f80b1f1887893d405355281bf7f3
samorai <oleg.samorai@gmail.com>
2017-05-09 18:47:53 +0300

    TYPO changing mongo.so to mongodb.so

--- .travis/scripts/install_php.sh
+++ .travis/scripts/install_php.sh
@@ -3,9 +3,8 @@ sudo touch /var/log/php-fpm.log
 sudo chmod 777 /var/log/php-fpm.log
 
 # Set custom config
-sudo pecl install mongo
 sudo ln -s $PWD/.travis/conf/php-fpm.conf ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
 echo 'always_populate_raw_post_data = -1' >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo 'date.timezone = "Europe/Kiev"' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
-echo "extension = mongo.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+echo "extension = mongodb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
